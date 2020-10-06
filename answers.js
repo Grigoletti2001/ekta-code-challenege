8adc1c943ebc84e169bf02944d2b1674fb14c88a
Joseph Grigoletti <jgrig2@gmail.com>
2020-10-06 15:01:59 -0500

    added questions

--- answers.js
+++ answers.js
@@ -1,1 +1,27 @@
 //Please put your answers here
+
+1. What is this code doing? How might it be improved?
+
+
+
+2. What is this code doing?
+
+
+
+3. What is this code doing?
+
+
+
+4.  I have an array of objects, they are currently untyped.
+ I'm unsure of how but mixed in with my normal objects (`{ id, name, date, createdAt }`), 
+ are `null` values. How would you eliminate the null values
+
+
+
+
+5.  In Angular 2+ how to you pass variable(s) from a parent to a child? answer in concept
+
+
+
+
+
